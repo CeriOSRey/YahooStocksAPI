bb4aa80e6b4243ea820736431f3e34df20ea1f88
Rey Cerio <rey.cerio@outlook.com>
2022-12-29 16:29:01 -0800

    completed StocksAPI SPM library

--- Sources/StocksAPI/Model/ChartRange.swift
+++ Sources/StocksAPI/Model/ChartRange.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+public enum ChartRange: String, CaseIterable {
+    case oneDay = "1d"
+    case oneWeek = "5d"
+    case oneMonth = "1mo"
+    case threeMonth = "3mo"
+    case sixMonth = "6mo"
+    case ytd
+    case oneYear = "1y"
+    case twoYear = "2y"
+    case fiveYear = "5y"
+    case tenYear = "10y"
+    case max
+    
+    public var interval: String {
+        switch self {
+        case .oneDay: return "1m"
+        case .oneWeek: return "5m"
+        case .oneMonth: return "90m"
+        case .threeMonth, .sixMonth, .ytd, .oneYear, .twoYear: return "1d"
+        case .fiveYear, .tenYear: return "1w"
+        case .max: return "3mo"
+        }
+    }
+}
--- Sources/StocksAPI/Model/Ticker.swift
+++ Sources/StocksAPI/Model/Ticker.swift
@@ -6,3 +6,53 @@
 //
 
 import Foundation
+
+public struct SearchTickerResponse: Decodable {
+    
+    enum CodingKeys: CodingKey {
+        case quotes
+        case finance
+    }
+    
+    enum FinanceKeys: CodingKey {
+        case error
+    }
+    
+    public let data: [Ticker]?
+    public let error: ErrorResponse?
+    
+    
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        data = try? container.decodeIfPresent([Ticker].self, forKey: .quotes)
+        error = try? container.nestedContainer(keyedBy: FinanceKeys.self, forKey: .finance)
+            .decodeIfPresent(ErrorResponse.self, forKey: .error)
+    }
+    
+}
+
+public struct Ticker: Codable, Identifiable, Hashable, Equatable {
+    
+    public let id = UUID()
+    
+    public let symbol: String
+    public let quoteType: String?
+    public let shortName: String?
+    public let longName: String?
+    public let sector: String?
+    public let industry: String?
+    public let exchDisp: String?
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.symbol = try container.decode(String.self, forKey: .symbol)
+        self.quoteType = try container.decodeIfPresent(String.self, forKey: .quoteType)
+        self.shortName = try container.decodeIfPresent(String.self, forKey: .shortName)
+        self.longName = try container.decodeIfPresent(String.self, forKey: .longName)
+        self.sector = try container.decodeIfPresent(String.self, forKey: .sector)
+        self.industry = try container.decodeIfPresent(String.self, forKey: .industry)
+        self.exchDisp = try container.decodeIfPresent(String.self, forKey: .exchDisp)
+    }
+    
+}
