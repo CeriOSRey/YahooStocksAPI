15d9ec1d9503335c3f3b711154114e6489a160b0
Rey Cerio <rey.cerio@outlook.com>
2022-12-29 16:14:04 -0800

    first commit

---
+++ Sources/StocksAPI/Model/Chart.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rey Cerio on 2022-12-27.
+//
+
+import Foundation
---
+++ Sources/StocksAPI/Model/ErrorResponse.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rey Cerio on 2022-12-13.
+//
+
+import Foundation
+
+public struct ErrorResponse: Codable {
+    
+    public let code: String
+    public let description: String
+    
+    public init(code:String, description: String) {
+        self.code = code
+        self.description = description
+    }
+}
